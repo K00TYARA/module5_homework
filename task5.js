35644ac53b97d0e78bea8a3c71805d3705c4cc91
K00TYARA <mr.kotlov.201@gmail.com>
2022-03-30 15:05:59 +0300

    Task5

---
+++ task5.js
@@ -0,0 +1,7 @@
+let arr = [1, 12, 45, 62, 19, 100];
+console.log(`Количество элементов: ${arr.length}`);
+
+console.log("Элементы:");
+for (let i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+}
